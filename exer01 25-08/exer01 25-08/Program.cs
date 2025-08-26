ae0b5d12350765bdd565d59117be9594f5c88a7a
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2025-08-25 21:26:29 -0300

    Add files via upload

---
+++ exer01 25-08/exer01 25-08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, f;
+
+            i = 11;
+            f = 250;
+            
+            Console.WriteLine("Esses são os números pares entre 11 e 250");
+            while (f >= i)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+                i++;
+            }
+        }
+    }
+}
