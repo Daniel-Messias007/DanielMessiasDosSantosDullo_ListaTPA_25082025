4221db8d59696f11fd418720dfbdfb0b9d964e54
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2025-08-25 21:32:06 -0300

    Add files via upload

---
+++ Exer05/Exer05/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n;
+            Console.WriteLine("Digite abaixo 15 números INTEIROS.");
+
+            Console.WriteLine(" ");
+            n1 = int.Parse(Console.ReadLine());
+            n = n1;
+
+            Console.WriteLine(" ");
+            n2 = int.Parse(Console.ReadLine());
+
+            if (n < n2) 
+            {
+                n = n2;
+            }
+
+            Console.WriteLine(" ");
+            n3 = int.Parse(Console.ReadLine());
+
+            if (n < n3)
+            {
+                n = n3;
+            }
+
+            Console.WriteLine(" ");
+            n4 = int.Parse(Console.ReadLine());
+
+            if (n < n4)
+            {
+                n = n4;
+            }
+
+            Console.WriteLine(" ");
+            n5 = int.Parse(Console.ReadLine());
+
+            if (n < n5)
+            {
+                n = n5;
+            }
+
+            Console.WriteLine(" ");
+            n6 = int.Parse(Console.ReadLine());
+
+            if (n < n6)
+            {
+                n = n6;
+            }
+
+
+            Console.WriteLine(" ");
+            n7 = int.Parse(Console.ReadLine());
+
+            if (n < n7)
+            {
+                n = n7;
+            }
+
+
+            Console.WriteLine(" ");
+            n8 = int.Parse(Console.ReadLine());
+
+            if (n < n8)
+            {
+                n = n8;
+            }
+
+
+            Console.WriteLine(" ");
+            n9 = int.Parse(Console.ReadLine());
+
+            if (n < n9)
+            {
+                n = n9;
+            }
+
+
+            Console.WriteLine(" ");
+            n10 = int.Parse(Console.ReadLine());
+
+            if (n < n10)
+            {
+                n = n10;
+            }
+
+
+            Console.WriteLine(" ");
+            n11 = int.Parse(Console.ReadLine());
+
+            if (n < n11)
+            {
+                n = n11;
+            }
+
+
+            Console.WriteLine(" ");
+            n12 = int.Parse(Console.ReadLine());
+
+            if (n < n12)
+            {
+                n = n12;
+            }
+
+
+            Console.WriteLine(" ");
+            n13 = int.Parse(Console.ReadLine());
+
+            if (n < n13)
+            {
+                n = n13;
+            }
+
+
+            Console.WriteLine(" ");
+            n14 = int.Parse(Console.ReadLine());
+
+            if (n < n14)
+            {
+                n = n14;
+            }
+
+
+            Console.WriteLine(" ");
+            n15 = int.Parse(Console.ReadLine());
+
+            if (n < n15)
+            {
+                n = n15;
+            }
+            Console.WriteLine("maior número foi: " + n);
+        }
+    }
+}
