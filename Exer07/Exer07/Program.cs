2adfb070f0c57001efdd3b85d84515f68c52478b
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2025-08-25 21:33:17 -0300

    Add files via upload

---
+++ Exer07/Exer07/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, f;
+
+            i = 1;
+            f = 100;
+
+            Console.WriteLine("Esses são os números de 1 até 100.");
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+            for (i = 11; i <= 20; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+            for (i = 21; i <= 30; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+            for (i = 31; i <= 40; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+            
+            for (i = 41; i <= 50; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+            for (i = 51; i <= 60; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+            for (i = 61; i <= 70; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+            for (i = 71; i <= 80; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+            for (i = 81; i <= 90; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+            for (i = 91; i <= 100; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine("MÚLTIPLO DE 10");
+
+        }
+    }
+}
