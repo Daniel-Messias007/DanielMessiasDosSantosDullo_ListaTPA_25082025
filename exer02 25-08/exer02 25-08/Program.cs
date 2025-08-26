1c1f79682f4cc3a7789089fca8fc6fed44aaf77b
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2025-08-25 21:29:04 -0300

    Add files via upload

---
+++ exer02 25-08/exer02 25-08/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 0;
+
+            Console.WriteLine("Digite dois números para fazer a ordem decrecente (apenas de números impares) entre eles ");
+            Console.WriteLine("Digite um número para o começo da ordem: ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número para o final: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+            while (x > y)
+            {
+                    if (x % 2 == 1) 
+                    {
+                        Console.WriteLine("{0}", x);
+                    }
+                x--;
+            }
+        }
+    }
+}
