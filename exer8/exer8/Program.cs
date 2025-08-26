72a8e994cf1c4a8d934f4b8b72c3fcfbc4c672fc
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2025-08-25 21:33:55 -0300

    Add files via upload

---
+++ exer8/exer8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            y = 1;
+
+            Console.WriteLine("Digite um número");
+            x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                y *= i;
+                Console.WriteLine(i + "!= " + y);
+            }
+        }
+    }
+}
