bdd840e75f47b542441e0073a673b7cc5b2d61ee
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2025-08-25 21:29:40 -0300

    Add files via upload

---
+++ exer03 25-08/exer03 25-08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r;
+
+            Console.WriteLine("Digite um número:");
+            n =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Aqui está a tabuada dele.");
+
+            for (int x = 0; x <= 10; x++)
+            {
+                r = n * x;
+                Console.WriteLine(x + " x " + n + " = " + r);
+            }
+            Console.ReadKey();
+        }
+    }
+}
